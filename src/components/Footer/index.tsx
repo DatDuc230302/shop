7816dc0dd695fa73ad0742cf421075bee1eea272
DatDuc230302 <110738474+DatDuc230302@users.noreply.github.com>
2023-06-19 14:49:32 +0700

    D

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,244 @@
 import classNames from 'classnames/bind';
 import style from './Footer.module.scss';
 
+const infoLists = [
+    {
+        title: 'G2A',
+        contents: ['About us', 'Newsroom', 'Contact', 'Careers', 'Reliability'],
+    },
+    {
+        title: 'COOPERATION',
+        contents: ['Marketing Partnership', 'Affiliate Program', 'Reviews'],
+    },
+    {
+        title: 'FOR BUYERS',
+        contents: ['Buyers Support', 'How to Buy', 'Stay Safe', 'Sitemap'],
+    },
+    {
+        title: 'FOR SELLERS',
+        contents: ['Sellers Support', 'How to Sell', 'Import via API', 'Business Center'],
+    },
+    {
+        title: 'USER TERMS & PRIVACY',
+        contents: ['Terms & Conditions', 'Privacy & Cookies'],
+    },
+    {
+        title: 'OUR PRODUCTS',
+        contents: ['G2A Plus', 'G2A News', 'G2A Loot'],
+    },
+];
+
+const locationLists = [
+    [
+        'G2A.COM Limited 31/F, Tower Two, Times Square, 1 Matheson Street',
+        'Causeway Bay, Hong Kong',
+        'Incorporation number: 2088957',
+        'Business registration number: 63264201',
+    ],
+    [
+        'Customer (support) services are granted by G2A PL Sp. z o.o.',
+        'Address:',
+        'G2A PL Sp. z o.o.',
+        '53 Emilii Plater Street',
+        '00-113 Warsaw',
+    ],
+];
+
 const cx = classNames.bind(style);
 function Footer() {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('body')}></div>
+                <div className={cx('body')}>
+                    <div className={cx('payment')}>
+                        <div className={cx('payment-methods')}>
+                            <div className={cx('paymentMethods-title')}>Payment methods:</div>
+                            <div className={cx('paymentMethods-list')}>
+                                <img src="https://www.g2a.com/static/assets/payment-methods/visa.svg" alt="" />
+                                <img src="https://www.g2a.com/static/assets/payment-methods/mastercard.svg" alt="" />
+                                <img src="https://www.g2a.com/static/assets/payment-methods/paysafecard.svg" alt="" />
+                                <img src="https://www.g2a.com/static/assets/payment-methods/discover.svg" alt="" />
+                                <img src="https://www.g2a.com/static/assets/payment-methods/paypal.svg" alt="" />
+                                and 200+ more
+                            </div>
+                        </div>
+                        <div className={cx('payment-languages')}>
+                            <div className={cx('paymentLanguages-img')}>
+                                <img src="https://www.g2a.com/static/assets/flags/englishus.svg" alt="" />
+                            </div>
+                            <span className={cx('paymentLanguages-title')}>United States (English) / USD</span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="18px"
+                                height="18px"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill="none"
+                                    strokeMiterlimit="10"
+                                    d="M16 10l-4 4-4-4"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    stroke="currentColor"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className={cx('info')}>
+                        <div className={cx('info-list')}>
+                            {infoLists.map((item: any, index: number) => (
+                                <div key={index} className={cx('info-item')}>
+                                    <span className={cx('infoItem-title')}>{item.title}</span>
+                                    {item.contents.map((content: any, index: number) => (
+                                        <span key={index} className={cx('infoItem-content')}>
+                                            {content}
+                                        </span>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                        <div className={cx('info-contact')}>
+                            <span className={cx('infoContact-title')}>Install the G2A app</span>
+                            <span className={cx('infoContact-content')}>Get great deals on games wherever you go!</span>
+                            <div className={cx('infoContact-vote')}>
+                                <div className={cx('infoContact-starts')}>
+                                    <svg
+                                        fill="#FFB400"
+                                        width={15}
+                                        height={15.2}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="#FFB400"
+                                        width={15}
+                                        height={15.2}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="#FFB400"
+                                        width={15}
+                                        height={15.2}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="#FFB400"
+                                        width={15}
+                                        height={15.2}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                                    </svg>
+                                    <svg
+                                        width={15}
+                                        height={15.2}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="url(#i5)"
+                                    >
+                                        <defs>
+                                            <linearGradient id="i5">
+                                                <stop
+                                                    stopOpacity="1"
+                                                    offset="50%"
+                                                    style={{ color: '#FFB400', fill: '#FFB400', stopColor: '#FFB400' }}
+                                                ></stop>
+                                                <stop
+                                                    stopOpacity="1"
+                                                    offset="50%"
+                                                    style={{ fill: '#D9D9D9', stopColor: '#D9D9D9' }}
+                                                ></stop>
+                                            </linearGradient>
+                                        </defs>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                                    </svg>
+                                </div>
+                                <span style={{ fontSize: '1.4rem', marginRight: 4 }}>
+                                    4.5 &nbsp;&nbsp;-&nbsp;&nbsp; 48,736{' '}
+                                </span>
+                                votes
+                            </div>
+                            <div className={cx('infoContact-get')}>
+                                <div className={cx('infoContact-getImg')}>
+                                    <img src="https://www.g2a.com/static/assets/images/badge_google_play.svg" alt="" />
+                                </div>
+                                <div className={cx('infoContact-getImg')}>
+                                    <img src="https://www.g2a.com/static/assets/images/badge_app_store.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('location')}>
+                        <div className={cx('location-detail')}>
+                            {locationLists.map((item: any, index: number) => (
+                                <div key={index} className={cx('location-list')}>
+                                    {item.map((title: any, index: number) => (
+                                        <span key={index} className={cx('locationList-item')}>
+                                            {title}
+                                        </span>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                        <div className={cx('location-socials')}>
+                            <img
+                                src="https://images.g2a.com/facebook/28x28/1x1x1/8e99c08cdbda/605ae42d7e696c259961a462"
+                                alt=""
+                            />
+                            <img
+                                src="https://images.g2a.com/twitter/28x28/1x1x1/225a9ced8d38/605ae47e46177c04b511d712"
+                                alt=""
+                            />
+                            <img
+                                src="https://images.g2a.com/youtube/28x28/1x1x1/6624e20dfc20/605ae4d746177c471a610dd2"
+                                alt=""
+                            />
+                            <img
+                                src="https://images.g2a.com/instagram/28x28/1x1x1/a28f82123e6b/605ae4aa7e696c2a5346b672"
+                                alt=""
+                            />
+                            <img
+                                src="https://images.g2a.com/linkedin/28x28/1x1x1/8223483aa633/605ae5127e696c2efa512f62"
+                                alt=""
+                            />
+                            <img
+                                src="https://images.g2a.com/twitch/28x28/1x1x1/9422ab6d9c45/605ae53646177c7b1b4eb952"
+                                alt=""
+                            />
+                            <img
+                                src="https://images.g2a.com/reddit/28x28/1x1x1/8a5840c01a34/605ae17746177c6d930f11c2"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className={cx('privacy')}>
+                        Use of this Web site constitutes acceptance of the
+                        <span className={cx('privacy-link')}> Terms and Conditions</span> and
+                        <span className={cx('privacy-link')}> Privacy policy</span>. All copyrights, trade marks,
+                        service marks belong to the corresponding owners.
+                    </div>
+                </div>
             </div>
         </div>
     );
